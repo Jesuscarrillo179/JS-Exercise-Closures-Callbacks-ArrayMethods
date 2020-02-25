6f2c8779418f0b38c0a2f14c596f51490006bdb4
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-02-25 16:46:18 -0600

    11 passing

--- index.js
+++ index.js
@@ -199,8 +199,12 @@ function getFullNames(runners){
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let capitalRunners = [];
+  runners.map((name) => {
+    return capitalRunners.push(name.first_name.toUpperCase());
+  })
+  return capitalRunners;
 }
 
 /**
